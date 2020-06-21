ee075fb58a8d15a7151cde6a116700c4b51d5b81
CalvoM <mwadimemakokha@ieee.org>
2020-06-21 17:14:53 +0300

    Get difference between files
    Slow but sure approach to getting difference

--- python-dev/main.py
+++ python-dev/main.py
@@ -1,16 +1,52 @@
 from typing import List, Set
 import math
-doc_1: List = ["The", "three", "mice"]
-doc_2: List = ["mice", "three", "The"]
+import sys
 
+
+@profile
+def get_file_lines(filename):
+    word_list: List = []
+    with open(file= filename, mode='r', encoding='windows-1252') as f:
+        word_list = f.readlines()
+    
+    return word_list
+
+
+@profile
+def get_words_from_file_lines(file_lines):
+    words_list: List =[]
+    for line in file_lines:
+        words_list += get_words_from_line(line)
+
+    return words_list
+
+
+@profile
+def get_words_from_line(line: str):
+    line_word_list: List = []
+    word_list: List = []
+    for ch in line:
+        if ch.isalnum():
+            word_list.append(ch)
+        elif len(word_list) > 0:
+            word = ''.join(word_list)
+            word.lower()
+            line_word_list.append(word)
+            word_list = []
+    return line_word_list
+
+
+@profile
 def get_word_frequency(word_list: List):
     freq: List = []
     doc_set: Set = set(word_list)
     for word in doc_set:
         freq.append((word,word_list.count(word)))
-    print(freq)
+
     return freq
 
+
+@profile
 def inner_product(L1: List, L2: List):
     sum=0
     for word, count in L1:
@@ -21,11 +57,17 @@ def inner_product(L1: List, L2: List):
     
     return sum
 
-
+@profile
+def get_degree_of_diff(f1,f2):
+    numerator = inner_product(f1,f2)
+    denominator = math.sqrt(inner_product(f1,f1) * inner_product(f2,f2))
+    return math.acos(numerator/denominator)
 
 if __name__ == "__main__":
-    w_freq = get_word_frequency(doc_1)
-    w_freq_2 = get_word_frequency(doc_2)
-    numerator = inner_product(w_freq, w_freq_2)
-    denominator = math.sqrt(inner_product(w_freq, w_freq) * inner_product(w_freq_2, w_freq_2))
-    print(math.acos(numerator/denominator))
+    file_words : List = get_file_lines(sys.argv[1])
+    file_words1 : List = get_file_lines(sys.argv[2])
+    words = get_words_from_file_lines(file_words)
+    words1 = get_words_from_file_lines(file_words1)
+    f1 = get_word_frequency(words)
+    f2 = get_word_frequency(words1)
+    print(get_degree_of_diff(f1,f2))
